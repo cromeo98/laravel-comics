0d3d6ad18db13e29afe0a76356db1241e10fa96b
cromeo98 <romeo.christian98@gmail.com>
2021-09-07 18:45:34 +0200

    npm install + scss and card in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,22 @@
 
     <h1>This is my content</h1>
 
-    @dd($comicsSeries)
+    <div class="jumbo">
+        THIS IS MY JUMBO
+    </div>
+
+
+
+    <div class="card-container">
+        THIS IS MY CARD SECTION
+        @foreach($comicsSeries as $comicsSerie)
+            <div class="card">
+                <img src="{{$comicsSerie['thumb']}}" alt="{{$comicsSerie['title']}}">
+                <h6>{{$comicsSerie['series']}}</h6>
+            </div>
+        @endforeach
+    </div>
+
+
 
 @endsection
