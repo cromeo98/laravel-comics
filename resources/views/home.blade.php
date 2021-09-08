1ba055787c8ed3403f1bf5532182d203182b76b4
cromeo98 <romeo.christian98@gmail.com>
2021-09-08 15:56:48 +0200

    end of exercise

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,15 +11,21 @@
 
 
 
-    <div class="card-container">
-        THIS IS MY CARD SECTION
-        @foreach($comicsSeries as $comicsSerie)
-            <div class="card">
-                <img src="{{$comicsSerie['thumb']}}" alt="{{$comicsSerie['title']}}">
-                <h6>{{$comicsSerie['series']}}</h6>
-            </div>
-        @endforeach
+    <div class="container">
+
+        <div class="card-container">
+
+            @foreach($comicsSeries as $comicsSerie)
+                <div class="card">
+                    <img src="{{$comicsSerie['thumb']}}" alt="{{$comicsSerie['title']}}">
+                    <h6>{{$comicsSerie['series']}}</h6>
+                </div>
+            @endforeach
+
+        </div>
+
     </div>
+
 
 
 
